457382f95565daeda843c64c2d9727975f57a2bb
Mateusz Kwapich <mateusz@kwapich.it>
2020-12-05 21:30:51 +0100

    Day 1: refactor to generalize

--- src/01_report_repair.rs
+++ src/01_report_repair.rs
@@ -1,24 +1,45 @@
 #[macro_use]
 extern crate scan_rules;
 
-use std::collections::BTreeSet;
+use multiset::HashMultiSet;
+
 use std::io::{self, BufRead};
+
+fn n_entries_that_sum_to_x(entries: &mut HashMultiSet<i32>, n: i32, x: i32) -> Option<Vec<i32>> {
+    if n == 1 {
+        if entries.contains(&x) {
+            return Some(vec![x]);
+        }
+    }
+    else {
+        let entries_copy = entries.clone();
+        for entry in entries_copy.iter() {
+            let complement = x - entry;
+            entries.remove(entry);
+            if let Some(mut ret) = n_entries_that_sum_to_x(entries, n - 1, complement) {
+                ret.push(*entry);
+                return Some(ret);
+            }
+            entries.insert(*entry);
+        }
+
+    }
+    return None;
+}
 
 fn main() {
     let stdin = io::stdin();
 
-    let mut entries = BTreeSet::new();
+    let mut entries = HashMultiSet::new();
 
     for line in stdin.lock().lines() {
         let_scan!(line.unwrap(); (let entry :i32));
         entries.insert(entry);
     }
 
-    for entry in &entries {
-        let matching_entry = 2020 - entry;
-        if entries.contains(&matching_entry) {
-            println!("{}", entry * matching_entry);
-            break;
-        }
+    if let Some(ret) = n_entries_that_sum_to_x(&mut entries, 2, 2020) {
+        println!("Product of two entries that sum up to 2020 is {}", ret[0] * ret[1]);
+    } else {
+        println!("No two entries sum to 2020");
     }
 }
