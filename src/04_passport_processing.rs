f4e1dffe11bd4578b6a38c80d950775ae4780e7a
Mateusz Kwapich <mateusz@kwapich.it>
2020-12-06 19:09:33 +0100

    Day 4: Passport Processing - refactor

--- src/04_passport_processing.rs
+++ src/04_passport_processing.rs
@@ -2,7 +2,7 @@
 #[macro_use]
 extern crate maplit;
 use anyhow::Result;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::io::{self, Read};
 
 fn main() -> Result<()> {
@@ -20,12 +20,13 @@ fn main() -> Result<()> {
     let stdin = io::stdin();
     stdin.lock().read_to_string(&mut buffer)?;
     for passport_data in buffer.split("\n\n") {
-        let mut present_fields = HashSet::new();
+        let mut parsed_passport_data = HashMap::new();
         for field_data in passport_data.split(char::is_whitespace) {
-            if let Some((field_name, _field_value)) = field_data.split_once(':') {
-                present_fields.insert(field_name);
+            if let Some((field_name, field_value)) = field_data.split_once(':') {
+                parsed_passport_data.insert(field_name, field_value);
             }
         }
+        let present_fields: HashSet<_> = parsed_passport_data.keys().copied().collect();
         if required_fields.is_subset(&present_fields) && present_fields.is_subset(&allowed_fields) {
             valid_count += 1;
         }
