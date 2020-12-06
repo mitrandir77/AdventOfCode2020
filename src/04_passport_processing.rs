71da0a431b227dbd5c494a2f1e9bf7432735ea40
Mateusz Kwapich <mateusz@kwapich.it>
2020-12-07 00:45:03 +0100

    Day 4: Passport Processing - part two
    A bit ugly, will refactor later.

--- src/04_passport_processing.rs
+++ src/04_passport_processing.rs
@@ -2,18 +2,54 @@
 #[macro_use]
 extern crate maplit;
 use anyhow::Result;
-use std::collections::{HashSet, HashMap};
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
 use std::io::{self, Read};
+
+fn validate_year(year: &str, lower_bound: i32, upper_bound: i32) -> bool {
+    if year.len() == 4 {
+        if let Ok(year) = year.parse::<i32>() {
+            return (lower_bound..=upper_bound).contains(&year);
+        }
+    }
+    false
+}
+
+fn validate_hair_colour(colour: &str) -> bool {
+    let re = Regex::new(r"^#[a-z0-9]{6}$").unwrap();
+    re.is_match(colour)
+}
+
+fn validate_eye_colour(colour: &str) -> bool {
+    let valid_colours = hashset! { "amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+    valid_colours.contains(colour)
+}
+
+fn validate_height(height: &str) -> bool {
+    let re = Regex::new(r"^(\d+)((cm)|(in))$").unwrap();
+    if let Some(captures) = re.captures(height) {
+        let height = captures[1].parse::<i32>().unwrap();
+        if &captures[2] == "in" {
+            (59..=76).contains(&height)
+        } else {
+            (150..=193).contains(&height)
+        }
+    } else {
+        false
+    }
+}
+
+fn validate_passport_id(id: &str) -> bool {
+    let re = Regex::new(r"^[0-9]{9}$").unwrap();
+    re.is_match(id)
+}
 
 fn main() -> Result<()> {
     let mut buffer = String::new();
 
     let required_fields = hashset! {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
     let optional_fields = hashset! {"cid"};
-    let allowed_fields: HashSet<_> = required_fields
-        .union(&optional_fields)
-        .copied()
-        .collect();
+    let allowed_fields: HashSet<_> = required_fields.union(&optional_fields).copied().collect();
 
     let mut valid_count = 0;
 
@@ -27,7 +63,16 @@ fn main() -> Result<()> {
             }
         }
         let present_fields: HashSet<_> = parsed_passport_data.keys().copied().collect();
-        if required_fields.is_subset(&present_fields) && present_fields.is_subset(&allowed_fields) {
+        if required_fields.is_subset(&present_fields)
+            && present_fields.is_subset(&allowed_fields)
+            && validate_year(parsed_passport_data.get("byr").unwrap(), 1920, 2002)
+            && validate_year(parsed_passport_data.get("iyr").unwrap(), 2010, 2020)
+            && validate_year(parsed_passport_data.get("eyr").unwrap(), 2020, 2030)
+            && validate_height(parsed_passport_data.get("hgt").unwrap())
+            && validate_hair_colour(parsed_passport_data.get("hcl").unwrap())
+            && validate_eye_colour(parsed_passport_data.get("ecl").unwrap())
+            && validate_passport_id(parsed_passport_data.get("pid").unwrap())
+        {
             valid_count += 1;
         }
     }
