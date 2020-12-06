b54e4f115b296ffa16bfb741986d6d5f4d7cb3c6
Mateusz Kwapich <mateusz@kwapich.it>
2020-12-06 18:33:38 +0100

    Day 3: Toboggan Trajectory - refactor

--- src/03_toboggan_trajectory.rs
+++ src/03_toboggan_trajectory.rs
@@ -1,18 +1,23 @@
-fn main() {
-    use std::io::{self, BufRead};
-    let stdin = io::stdin();
+use std::io::{self, BufRead};
+
+fn check_slope(lines: &Vec<String>, right: usize, down: usize) -> i32 {
     let mut x = 0; // current tobogan position
     let mut tree_count = 0;
-
-    let mut lines = stdin.lock().lines();
-    lines.next();
-    for line in lines {
-        let line = line.unwrap();
-        x += 3;
-        x %= line.len();
-        if line.chars().nth(x) == Some('#') {
-            tree_count += 1;
+    for (line_no, line) in lines.iter().skip(down).enumerate() {
+        if line_no % down == 0 {
+            x += right;
+            x %= line.len();
+            if line.chars().nth(x) == Some('#') {
+                tree_count += 1;
+            }
         }
     }
-    println!("{}", tree_count);
+    return tree_count;
+}
+
+fn main() {
+    let stdin = io::stdin();
+
+    let lines: Vec<_> = stdin.lock().lines().map(|x| x.unwrap()).collect();
+    println!("{}", check_slope(&lines, 3, 1));
 }
