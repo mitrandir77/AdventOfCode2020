c3d9eed6638cc562d0ac639d05882aa0649357ae
Mateusz Kwapich <mateusz@kwapich.it>
2020-12-24 10:35:04 +0100

    Day 15: Rambunctious Recitation - part 2

--- src/15_rambunctious_recitation.rs
+++ src/15_rambunctious_recitation.rs
@@ -9,7 +9,7 @@ fn main() {
         spoken_before.insert(*number, turn);
     }
     let mut prev_number = *numbers.as_slice().last().unwrap();
-    for turn in numbers.len()..2020 {
+    for turn in numbers.len()..30000000 {
         let current_number = match spoken_before.get(&prev_number) {
             Some(previous_turn) => turn - 1 - previous_turn,
             None => 0,
